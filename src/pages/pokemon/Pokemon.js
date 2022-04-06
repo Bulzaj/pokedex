49a04d0a9e1bdc3b0ec3c7b53625c3c382a6bbcf
Bulzaj <ernestsado94@gmail.com>
2022-04-06 15:38:13 +0200

    refactor species desc section

--- src/pages/pokemon/Pokemon.js
+++ src/pages/pokemon/Pokemon.js
@@ -4,7 +4,7 @@ import { useParams } from "react-router-dom";
 import usePokemonSpecies from "../../hooks/usePokemonSpecies";
 import useAbilities from "../../hooks/useAbilities";
 import { capitalizeFirstLetter } from "../../utils";
-import SpeciesDesc from "../../components/species-desc/speciesDesc";
+import SpeciesDescSection from "../../components/sections/species-desc-section/speciesDescSection";
 import GeneralInfoSection from "../../components/sections/general-info-section/generalInfoSection";
 import AbilitiesInfoSection from "../../components/sections/abilities-info-section/abilitiesInfoSection";
 import StatsInfo from "../../components/stats-info/statsInfo";
@@ -132,7 +132,7 @@ const Pokemon = function () {
           <PokemonTypesSection types={types} />
         </Col>
         <Col>
-          <SpeciesDesc flavorTextEntries={flavorTextEntries} />
+          <SpeciesDescSection flavorTextEntries={flavorTextEntries} />
           <GeneralInfoSection info={basicInfo} />
           <AbilitiesInfoSection
             abilities={abilities}
