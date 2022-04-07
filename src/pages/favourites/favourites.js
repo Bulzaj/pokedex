17abde17303aa99e176519a67951f36c5e67dd00
Bulzaj <ernestsado94@gmail.com>
2022-04-07 11:33:06 +0200

    change Pokemon card content

--- src/pages/favourites/favourites.js
+++ src/pages/favourites/favourites.js
@@ -1,5 +1,11 @@
+import { Container } from "react-bootstrap";
+
 const Favourites = function () {
-  return <>Favourites Page</>;
+  return (
+    <Container>
+      <h3 className="display-3">Favourites</h3>
+    </Container>
+  );
 };
 
 export default Favourites;
