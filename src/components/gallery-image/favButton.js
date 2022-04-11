63840e804726ceb095dc2b2dad27f8f1b7f5913c
Bulzaj <ernestsado94@gmail.com>
2022-04-11 18:13:10 +0200

    create favourites hook

--- src/components/gallery-image/favButton.js
+++ src/components/gallery-image/favButton.js
@@ -1,17 +1,25 @@
+import { useEffect, useState } from "react";
 import { Button } from "react-bootstrap";
+import useFavourites from "../../hooks/useFavourites";
 
 const FavButton = function (props) {
   const { pokemonName } = props;
+  const { favourites, add, remove } = useFavourites();
+  const [added, setAdded] = useState(false);
 
-  let btnName = "Add to favourites";
-  let btnVariant = "primary";
-  if (true) {
-    btnName = "Remove from favourites";
-    btnVariant = "danger";
-  }
+  useEffect(() => {
+    if (favourites.includes(pokemonName)) return setAdded(true);
+    return setAdded(false);
+  }, [favourites]);
+
+  const btnName = added ? "Remove from favourites" : "Add to favourites";
+
+  const btnVariant = added ? "danger" : "primary";
 
   const handleFavouritesClick = function (e, pokemonName) {
     e.stopPropagation();
+    if (!added) return add(pokemonName);
+    return remove(pokemonName);
   };
 
   return (
--- src/hooks/useFavourites.js
+++ src/hooks/useFavourites.js
@@ -1,3 +1,36 @@
-const useFavourites = function () {};
+import { useEffect, useState } from "react";
+
+const useFavourites = function () {
+  const [favourites, setFavourites] = useState(() => getFavourites());
+
+  useEffect(() => save(favourites), [favourites]);
+
+  const add = function (pokemonName) {
+    if (favourites.includes(pokemonName)) return;
+    const favouritesArr = getFavourites();
+    favouritesArr.push(pokemonName);
+    setFavourites(favouritesArr);
+  };
+
+  const remove = function (pokemonName) {
+    if (!favourites.includes(pokemonName)) return;
+    const favouritesArr = getFavourites();
+    const newArray = favouritesArr.filter(
+      (favourite) => favourite !== pokemonName
+    );
+    setFavourites(newArray);
+  };
+
+  return { favourites, add, remove };
+};
+
+const getFavourites = function () {
+  if (!localStorage.getItem("favourites")) return [];
+  return JSON.parse(localStorage.getItem("favourites"));
+};
+
+const save = function (favourites) {
+  localStorage.setItem("favourites", JSON.stringify(favourites));
+};
 
 export default useFavourites;
