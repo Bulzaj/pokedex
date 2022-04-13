3f5c1931e0c7cdf2cb4b1775a96c02b79598e8b8
Bulzaj <ernestsado94@gmail.com>
2022-04-13 15:32:05 +0200

    feature - create landing page with hero section

---
+++ src/components/top-navbar/pokemonListLink.js
@@ -0,0 +1,7 @@
+import { Nav } from "react-bootstrap";
+
+const PokemonListLink = function () {
+  return <Nav.Link href="/pokemon-list">Pokemon list</Nav.Link>;
+};
+
+export default PokemonListLink;
