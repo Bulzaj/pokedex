93796a80e8bb28dc0e522921521fceb16e419af5
Bulzaj <ernestsado94@gmail.com>
2022-04-17 18:42:42 +0200

    test - add "getCollectionTypeItem" test suites

--- src/components/collection/collection.test.js
+++ src/components/collection/collection.test.js
@@ -1,5 +1,6 @@
 import { render, screen } from "@testing-library/react";
 import CollectionPicker from "./collectionPicker";
+import getCollectionTypeItem from "./getCollectionTypeItem";
 
 describe("Collection picker component", () => {
   test('renders "Gallery" title', () => {
@@ -14,6 +15,27 @@ describe("Collection picker component", () => {
   });
   test('renders "Cards" title', () => {
     render(<CollectionPicker selectedType="cards" />);
+    const el = screen.getByText("Cards");
+    expect(el).toBeInTheDocument();
+  });
+});
+
+describe("Get collection type item function", () => {
+  test("returns gallery item", () => {
+    const result = getCollectionTypeItem("gallery");
+    render(result);
+    const el = screen.getByText("Gallery");
+    expect(el).toBeInTheDocument();
+  });
+  test("returns list item", () => {
+    const result = getCollectionTypeItem("list");
+    render(result);
+    const el = screen.getByText("List");
+    expect(el).toBeInTheDocument();
+  });
+  test("returns cards item", () => {
+    const result = getCollectionTypeItem("cards");
+    render(result);
     const el = screen.getByText("Cards");
     expect(el).toBeInTheDocument();
   });
